184fac993c23fd0453b88e779fd1834548de7d8e
fatma idris <fatma18650@gmail.com>
2023-09-21 17:26:04 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- * rot13 -  encodes a string using rot13
- * @s: the string targeted
- * Return: returns the encoded string
-*/
-
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
 char *rot13(char *s)
 {
 	int i, j;
 
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
-	char *ptr = s;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; rot13[j] < 52; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == rot13[j])
+			if (s[i] == a[j])
 			{
-				s[i] = ROT13[j];
+				s[i] = b[j];
 				break;
 			}
 		}
-
 	}
+
 	return (s);
 }
