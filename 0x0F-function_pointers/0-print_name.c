ab69ccdb4ab6bcf63ab03e8eef00c36b4cf5cf5d
Fatma <fatma18650@gmail.com>
2023-10-12 00:55:45 +0000

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name
+* @name: the string ame
+* @f: parameter
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *));
+{
+	if (name && f)
+		f(name);
+}
