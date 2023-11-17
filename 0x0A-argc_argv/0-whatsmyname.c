07a8aebcf53e74c5de19a7e1fc3b294ab80c8fe1
fatmaidris <fatma18650@gmail.com>
2023-11-17 15:59:37 -0800

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print pogram's name
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
