241243ad0d0c6a4ca285b116f5caa89146a312ba
fatmaidris <fatma18650@gmail.com>
2024-01-25 17:24:03 -0800

    done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * delete_dnodeint_at_index - deletes the node at index index of a
- * 				dlistint_t linked list
+ *				dlistint_t linked list
  * @head: a pointer
  * @index: the index of the node to delete
  *
